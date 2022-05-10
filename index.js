4b2ca5f5f3b307e3ac084e9faa650a70e4b0e7cc
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-05-10 19:33:03 +0000

    check inputs

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// Take given arrays and join them into one array
+// New array must be sorted in ascending order
+// [0, 3, 3, 4, 4, 30, 31]
 
+function mergeSortedArrays(arr1, arr2) {
+  new_array = []
+  // check inputs
+  if (arr1 === 0) {
+    return arr2;
+  }
+  if (arr2 === 0) {
+    return arr1;
+  }   
+
+  return new_array
+}
 
 mergeSortedArrays([0,3,4,31], [3,4,6,30]);
