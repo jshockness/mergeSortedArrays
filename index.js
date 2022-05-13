35f6785d2e94e1093b6c2f8ba4dda37842dba1fb
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-05-13 15:57:22 +0000

    solution

--- index.js
+++ index.js
@@ -3,16 +3,37 @@
 // [0, 3, 3, 4, 4, 30, 31]
 
 function mergeSortedArrays(arr1, arr2) {
-  new_array = []
+  const new_array = []
+  let array1Item = (arr1[0])
+  let array2Item = (arr2[0])
+  i = 1
+  j = 1
+  
   // check inputs
   if (arr1 === 0) {
     return arr2;
   }
   if (arr2 === 0) {
     return arr1;
-  }   
+  }
+
+  while (array1Item || array2Item) {
+    console.log(array1Item, array2Item)
+    if ( !array2Item || array1Item < array2Item) {
+      new_array.push(array1Item)
+      array1Item = arr1[i];
+      i++;
+    } else {
+      new_array.push(array2Item);
+      array2Item =arr2[j];
+      j++;
+    }
+  }
+  // new_array.push(arr1)
+  // new_array.push(arr2)
+  // new_array.sort()
 
   return new_array
 }
 
-mergeSortedArrays([0,3,4,31], [3,4,6,30]);
+console.log(mergeSortedArrays([0,3,4,31], [3,4,6,30]));
